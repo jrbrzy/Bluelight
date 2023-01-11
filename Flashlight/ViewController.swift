fc620d4869640d18eeeadfa040c8036f514fd26d
Jairo Munoz <jrbrzy@icloud.com>
2023-01-11 10:04:51 -0700

    MVP BlueLights

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func onButtonTap(_ sender: Any) {
+        self.view.backgroundColor = UIColor.blue
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func ResetLight(_ sender: Any) {
+        self.view.backgroundColor = UIColor.white
+    }
+    
 }
 
